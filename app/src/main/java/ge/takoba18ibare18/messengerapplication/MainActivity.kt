621ec95055ad17896c066b241ccf5ecb9632c6fb
unknown <ibare18@freeuni.edu.ge>
2021-08-10 00:21:52 +0400

    configured firebase and drawables

--- app/src/main/java/ge/takoba18ibare18/messengerapplication/MainActivity.kt
+++ app/src/main/java/ge/takoba18ibare18/messengerapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package ge.takoba18ibare18.messengerapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+        myRef.setValue("Hello, World!")
     }
 }
